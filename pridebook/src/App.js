83ca1d0923aebc95282fff41ff505fae5fcbc27e
Tgleason1980 <terryscoding@gmail.com>
2020-03-29 00:52:27 -0500

    added routing and Login Page

--- pridebook/src/App.js
+++ pridebook/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import NavBar from "./components/NavBar/NavBar";
-import Header from './components/Header/Header';
-
+import LoginPage from './components/LoginPage/LoginPage';
+import { Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <NavBar/>
+      {/* <LoginPage/> */}
+      <Route exact path="/" component={LoginPage}/>
+      {/* <Route path="/contact" component={Contact}/>
+      <Route path="/about" component={About}/> */}
       </div>
   );
 }
--- pridebook/src/components/NavBar/NavBar.js
+++ pridebook/src/components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './NavBarStyles.css';
-
+import {Link }from 'react-router-dom';
 const NavBar = () =>{
     return( 
     <div className="Nav">
-        <h4><a href="/">Home</a></h4>
-        <h4><a href="chat">Chat</a></h4>
-        <h4><a href="profile">Profile</a></h4>
+        <h4><Link to="/Home" className="Link">Home</Link></h4>
+        <h4><Link to="/chat" className="Link">Chat</Link></h4>
+        <h4><Link to="/profile" className="Link">Profile</Link></h4>
     </div>)
 };
 
